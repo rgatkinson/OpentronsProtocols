8e873d293137dd1b1fc08169eeb060b3a384a7d1
Robert Atkinson <bob@theatkinsons.org>
2019-09-28 13:02:15 -0700

    add interval

--- Pairwise-Interaction-Combined.py
+++ Pairwise-Interaction-Combined.py
@@ -7,6 +7,7 @@ import json
 import math
 import numpy
 from abc import abstractmethod
+from functools import wraps
 from numbers import Number
 from typing import List
 from opentrons import labware, instruments, robot, modules, types
@@ -75,6 +76,436 @@ allow_carryover = allow_blow_elision
 ##                                                                                                                    ##
 ########################################################################################################################
 ########################################################################################################################
+
+########################################################################################################################
+# Interval, adapted from pyinterval
+########################################################################################################################
+
+# region Floating point helper
+
+# noinspection PyPep8Naming
+class fpu(object):
+    float = float
+    _min = min
+    _max = max
+    infinity = float('inf')
+    nan = infinity / infinity
+
+    _fe_upward = None
+    _fe_downward = None
+    _fegetround = None
+    _fesetround = None
+
+    class NanException(ValueError):
+        # Exception thrown when an unwanted nan is encountered.
+        pass
+
+    @classmethod
+    def _init_libm(cls):  # pragma: nocover
+        import platform
+        processor = platform.processor()
+        if processor == 'powerpc':
+            cls._fe_upward, cls._fe_downward = 2, 3
+        elif processor == 'sparc':
+            cls._fe_upward, cls._fe_downward = 0x80000000, 0xC0000000
+        else:
+            cls._fe_upward, cls._fe_downward = 0x0800, 0x0400
+
+        from ctypes import cdll
+        from ctypes.util import find_library
+        libm = cdll.LoadLibrary(find_library('m'))
+        cls._fegetround, cls._fesetround = libm.fegetround, libm.fesetround
+
+    @classmethod
+    def _init_msvc(cls):  # pragma: nocover
+        from ctypes import cdll
+        controlfp = cdll.msvcrt._controlfp
+
+        def local_fegetround():
+            return controlfp(0, 0)
+
+        def local_fesetround(flag):
+            controlfp(flag, 0x300)
+
+        cls._fe_upward, cls._fe_downward = 0x0200, 0x0100
+        cls._fegetround = local_fegetround
+        cls._fesetround = local_fesetround
+
+    @classmethod
+    def init(cls):  # pragma: nocover
+        for f in cls._init_libm, cls._init_msvc:
+            # noinspection PyBroadException
+            try:
+                f()
+            except:
+                pass
+            else:
+                break
+        else:
+            import warnings
+            warnings.warn("Cannot determine FPU control primitives. The fpu module is not correctly initialized.", stacklevel=2)
+
+    @staticmethod
+    def isnan(x):
+        return x != x
+
+    @classmethod
+    def down(cls, f):
+        # Perform a computation with the FPU rounding downwards
+        saved = cls._fegetround()
+        try:
+            cls._fesetround(cls._fe_downward)
+            return f()
+        finally:
+            cls._fesetround(saved)
+
+    @classmethod
+    def up(cls, f):
+        # Perform a computation with the FPU rounding upwards.
+        saved = cls._fegetround()
+        try:
+            cls._fesetround(cls._fe_upward)
+            return f()
+        finally:
+            cls._fesetround(saved)
+
+    @classmethod
+    def ensure_nonan(cls, x):
+        if cls.isnan(x):
+            raise cls.NanException
+        return x
+
+    @classmethod
+    def min(cls, values):
+        try:
+            return cls._min(cls.ensure_nonan(x) for x in values)
+        except cls.NanException:
+            return cls.nan
+
+    @classmethod
+    def max(cls, values):
+        try:
+            return cls._max(cls.ensure_nonan(x) for x in values)
+        except cls.NanException:
+            return cls.nan
+
+    @staticmethod
+    def isinteger(n):
+        return isinstance(n, int)
+
+    @classmethod
+    def power_rn(cls, x, n):
+        # Raise x to the n-th power (with n positive integer), rounded to nearest.
+        assert cls.isinteger(n) and n >= 0
+        value = ()
+        while n > 0:
+            n, y = divmod(n, 2)
+            value = (y, value)
+        result = 1.0
+        while value:
+            y, value = value
+            if y:
+                result = result * result * x
+            else:
+                result = result * result
+        return result
+
+    @classmethod
+    def power_ru(cls, x, n):
+        # Raise x to the n-th power (with n positive integer), rounded toward +inf.
+        if x >= 0:
+            return cls.up(lambda: cls.power_rn(x, n))
+        elif n % 2:
+            return - cls.down(lambda: cls.power_rn(-x, n))
+        else:
+            return cls.up(lambda: cls.power_rn(-x, n))
+
+    @classmethod
+    def power_rd(cls, x, n):
+        # Raise x to the n-th power (with n positive integer), rounded toward -inf.
+        if x >= 0:
+            return cls.down(lambda: cls.power_rn(x, n))
+        elif n % 2:
+            return - cls.up(lambda: cls.power_rn(-x, n))
+        else:
+            return cls.down(lambda: cls.power_rn(-x, n))
+
+
+fpu.init()
+# endregion
+
+# region interval
+def coercing(f):
+    @wraps(f)
+    def wrapper(self, other):
+        try:
+            return f(self, self.cast(other))
+        except self.ScalarError:
+            return NotImplemented
+    return wrapper
+
+def comp_by_comp(f):
+    @wraps(f)
+    def wrapper(self, other):
+        try:
+            return self._canonical(
+                self.Component(*f(x, y))
+                for x in self
+                for y in self.cast(other))
+        except self.ScalarError:
+            return NotImplemented
+    return wrapper
+
+class Metaclass(type):
+    def __getitem__(self, arg):
+        return self(arg)
+
+
+# noinspection PyPep8Naming
+class interval(tuple, metaclass=Metaclass):
+
+    def __new__(cls, *args):
+        if len(args) == 1 and isinstance(args[0], cls):
+            return args[0]
+
+        def make_component(x, y=None):
+            if y is None:
+                return cls.cast(x)
+            else:
+                return cls.hull((cls.cast(x), cls.cast(y)))
+
+        def process(x):
+            try:
+                return make_component(*x if hasattr(x, '__iter__') else (x,))
+            except:
+                raise cls.ComponentError("Invalid interval component: " + repr(x))
+
+        return cls.union(process(x) for x in args)
+
+    def __getnewargs__(self):
+        return tuple(tuple(c) for c in self)
+
+    @classmethod
+    def new(cls, components):
+        return tuple.__new__(cls, components)
+
+    @classmethod
+    def cast(cls, x):
+        if isinstance(x, cls):
+            return x
+        try:
+            y = fpu.float(x)
+        except:
+            raise cls.ScalarError("Invalid scalar: " + repr(x))
+        if fpu.isinteger(x) and x != y:
+            # Special case for an integer with more bits than in a float's mantissa
+            if x > y:
+                return cls.new((cls.Component(y, fpu.up(lambda: y + 1)),))
+            else:
+                return cls.new((cls.Component(fpu.down(lambda: y - 1), y),))
+        return cls.new((cls.Component(y, y),))
+
+    @classmethod
+    def function(cls, f):
+        @wraps(f)
+        def wrapper(x):
+            return cls._canonical(cls.Component(*t) for c in cls.cast(x) for t in f(c))
+        return wrapper
+
+    @classmethod
+    def _canonical(cls, components):
+        from operator import itemgetter
+        components = [c for c in components if c.infimum <= c.supremum]
+        components.sort(key=itemgetter(0))
+        value = []
+        for c in components:
+            if not value or c.infimum > value[-1].supremum:
+                value.append(c)
+            elif c.supremum > value[-1].supremum:
+                value[-1] = cls.Component(value[-1].infimum, c.supremum)
+        return cls.new(value)
+
+    @classmethod
+    def union(cls, intervals):
+        return cls._canonical(c for i in intervals for c in i)
+
+    @classmethod
+    def hull(cls, intervals):
+        components = [c for i in intervals for c in i]
+        return cls.new((cls.Component(fpu.min(c.infimum for c in components), fpu.max(c.supremum for c in components)),))
+
+    @property
+    def components(self):
+        return (self.new((x,)) for x in self)
+
+    @property
+    def midpoint(self):
+        return self.new(self.Component(x, x) for x in (sum(c) / 2 for c in self))
+
+    @property
+    def extrema(self):
+        return self._canonical(self.Component(x, x) for c in self for x in c)
+
+    def __repr__(self):
+        return self.format("%r")
+
+    def __str__(self):
+        return self.format("%s")
+
+    def format(self, fs):
+        return type(self).__name__ + '(' + ', '.join('[' + ', '.join(fs % x for x in sorted(set(c))) + ']' for c in self) + ')'
+
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return self.new(self.Component(-x.supremum, -x.infimum) for x in self)
+
+    @comp_by_comp
+    def __add__(x, y):
+        return (fpu.down(lambda: x.infimum + y.infimum), fpu.up(lambda: x.supremum + y.supremum))
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return (-self) + other
+
+    @comp_by_comp
+    def __mul__(x, y):
+        return (
+            fpu.down(lambda: fpu.min((x.infimum * y.infimum, x.infimum * y.supremum, x.supremum * y.infimum, x.supremum * y.supremum))),
+            fpu.up  (lambda: fpu.max((x.infimum * y.infimum, x.infimum * y.supremum, x.supremum * y.infimum, x.supremum * y.supremum))))
+
+    def __rmul__(self, other):
+        return self * other
+
+    @coercing
+    def __div__(self, other):
+        return self * other.inverse()
+
+    __truediv__ = __div__
+
+    @coercing
+    def __rdiv__(self, other):
+        return self.inverse() * other
+
+    __rtruediv__ = __rdiv__
+
+    def __pow__(self, n):
+        if not fpu.isinteger(n):
+            return NotImplemented
+        if n < 0:
+            return (self ** -n).inverse()
+        if n % 2:
+            def pow(c):
+                return (fpu.power_rd(c.infimum, n), fpu.power_ru(c.supremum, n))
+        else:
+            def pow(c):
+                if c.infimum > 0:
+                    return (fpu.power_rd(c.infimum, n), fpu.power_ru(c.supremum, n))
+                if c.supremum < 0:
+                    return (fpu.power_rd(c.supremum, n), fpu.power_ru(c.infimum, n))
+                else:
+                    return (0.0, fpu.max(fpu.power_ru(x, n) for x in c))
+        return self._canonical(self.Component(*pow(c)) for c in self)
+
+    @comp_by_comp
+    def __and__(x, y):
+        return (fpu.max((x.infimum, y.infimum)), fpu.min((x.supremum, y.supremum)))
+
+    def __rand__(self, other):
+        return self & other
+
+    @coercing
+    def __or__(self, other):
+        return self.union((self, other))
+
+    def __ror__(self, other):
+        return self | other
+
+    @coercing
+    def __contains__(self, other):
+        return all(any(x.infimum <= y.infimum and y.supremum <= x.supremum for x in self) for y in other)
+
+    def __abs__(self):
+        return type(self)[0, fpu.infinity] & (self | (-self))
+
+    class ComponentError(ValueError):
+        pass
+
+    class ScalarError(ValueError):
+        pass
+
+    class Component(tuple):
+        def __new__(cls, inf, sup):
+            if fpu.isnan(inf) or fpu.isnan(sup):
+                return tuple.__new__(cls, (-fpu.infinity, +fpu.infinity))
+            return tuple.__new__(cls, (inf, sup))
+
+        @property
+        def infimum(self):
+            return self[0]
+
+        @property
+        def supremum(self):
+            return self[1]
+
+        @property
+        def infimum_inv(self):
+            return fpu.up(lambda: 1 / self.infimum)
+
+        @property
+        def supremum_inv(self):
+            return fpu.down(lambda: 1 / self.supremum)
+
+    def newton(self, f, p, maxiter=10000, tracer_cb=None):
+        if tracer_cb is None:
+            def tracer_cb(tag, interval):
+                pass
+
+        def step(x, i):
+            return (x - f(x) / p(i)) & i
+
+        def some(i):
+            yield i.midpoint
+            for x in i.extrema.components:
+                yield x
+
+        def branch(current):
+            tracer_cb('branch', current)
+            for n in range(maxiter):
+                previous = current
+                for anchor in some(current):
+                    current = step(anchor, current)
+                    if current != previous:
+                        tracer_cb('step', current)
+                        break
+                else:
+                    return current
+                if not current:
+                    return current
+                if len(current) > 1:
+                    return self.union(branch(c) for c in current.components)
+            tracer_cb("abandon", current)
+            return self.new(())
+
+        return self.union(branch(c) for c in self.components)
+
+    def inverse(c):
+        if c.infimum <= 0 <= c.supremum:
+            return ((-fpu.infinity, c.infimum_inv if c.infimum != 0 else -fpu.infinity),
+                    (c.supremum_inv if c.supremum != 0 else +fpu.infinity, +fpu.infinity))
+        else:
+            return (c.supremum_inv, c.infimum_inv),
+
+
+interval.inverse = interval.function(getattr(interval.inverse, '__func__', interval.inverse))
+del coercing, comp_by_comp, Metaclass
+# endregion
 
 ########################################################################################################################
 # Well enhancements
