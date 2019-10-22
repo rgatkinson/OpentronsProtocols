7a7db882712c3affe25291315e325eaf2068526d
Robert Atkinson <bob@theatkinsons.org>
2019-10-22 15:12:36 -0700

    use sqrt(5)x instead 5x dilution as run

--- allura_red_dilution_series.py
+++ allura_red_dilution_series.py
@@ -2235,11 +2235,11 @@ def verify_well_locations(well_list: List[Well], pipette: EnhancedPipette):
 ########################################################################################################################
 
 # Tip usage
-p10_start_tip = 'A1'
-p50_start_tip = 'A3'
-config.trash_control = False
+p10_start_tip = 'A2'
+p50_start_tip = 'A1'
+config.trash_control = True
 
-stock_volume = 1000
+stock_volume = 630
 
 ########################################################################################################################
 # Labware
@@ -2259,19 +2259,22 @@ p50.start_at_tip(tips300a[p50_start_tip])
 
 # All the labware containers
 eppendorf_1_5_rack = labware.load('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', 5, label='eppendorf_1_5_rack')
-plate = labware.load('biorad_96_wellplate_200ul_pcr', 6, label='plate')
+plateA = labware.load('biorad_96_wellplate_200ul_pcr', 6, label='plateA')
+plateB = labware.load('biorad_96_wellplate_200ul_pcr', 3, label='plateB')
 trough = labware.load('usascientific_12_reservoir_22ml', 9, label='trough')
 
 # Name specific places in the labware containers
 water = trough['A1']
 initial_stock = eppendorf_1_5_rack['A1']
-dilutions = eppendorf_1_5_rack.rows(1)
+dilutions = eppendorf_1_5_rack.rows(1) + eppendorf_1_5_rack.rows(2)  # 12 in all
 
 # Define geometries
 Eppendorf1point5mlTubeGeometry(initial_stock)
 for tube in dilutions:
     Eppendorf1point5mlTubeGeometry(tube)
-for well in plate.wells():
+for well in plateA.wells():
+    Biorad96WellPlateWellGeometry(well)
+for well in plateB.wells():
     Biorad96WellPlateWellGeometry(well)
 
 # Remember initial liquid names and volumes
@@ -2286,8 +2289,8 @@ del well
 # Dilutions
 ########################################################################################################################
 
-dilution_volume = 500
-dilution_factor = 5
+dilution_volume = 600
+dilution_factor = math.sqrt(5)  # yes, that's correct
 
 dilution_source_volume = dilution_volume / dilution_factor
 dilution_water_volume = dilution_volume - dilution_source_volume
@@ -2296,7 +2299,7 @@ def make_dilutions():
     log('transferring water for dilutions')
     p50.transfer(dilution_water_volume, water, dilutions, new_tip='once', trash=config.trash_control)
 
-    sources = WellSeries([initial_stock]) + dilutions[0:5]
+    sources = WellSeries([initial_stock]) + dilutions[0:-1]
     destinations = dilutions
     for source, destination in zip(sources, destinations):
         log(f'diluting from {source.get_name()} to {destination.get_name()}')
@@ -2304,13 +2307,21 @@ def make_dilutions():
         p50.layered_mix([destination])
 
 def plate_dilutions():
+    log('plating dilutions')
     tubes_to_plate = WellSeries([initial_stock]) + dilutions
     volumes = [50, 25, 10, 5]
     num_replicates = 3
     for row, source_tube in enumerate(tubes_to_plate):
+        log(f'plating {source_tube.get_name()}')
+        if row < 8:
+            plate = plateA
+            row_delta = 0
+        else:
+            plate = plateB
+            row_delta = 8
         for iVolume, volume in enumerate(volumes):
             col_first = iVolume * num_replicates
-            destination_wells = plate.rows(row)[col_first:col_first+num_replicates]
+            destination_wells = plate.rows(row-row_delta)[col_first:col_first+num_replicates]
             p = p10 if volume <= 10 else p50
             if not p.has_tip:  # make sure we have a tip. can reuse so long as we keep the same source tube
                 p.pick_up_tip()
