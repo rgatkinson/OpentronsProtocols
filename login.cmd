7dfd1b08167e106946a313ecd0d2e93dfbbd966e
Robert Atkinson <bob@theatkinsons.org>
2019-10-28 10:12:37 -0700

    commit to packages

---
+++ login.cmd
@@ -0,0 +1,4 @@
+::
+:: login.cmd
+::
+ssh -i c:\users\bob\clarence_ssh_key root@clarence.lutzlab.org
---
+++ push.cmd
@@ -0,0 +1,4 @@
+::
+:: push.cmd
+::
+scp -i c:\users\bob\clarence_ssh_key -r atkinson root@clarence.lutzlab.org:/data/packages/usr/local/lib/python3.6/site-packages
