18b783796ac7232458dd96cc2105bec0b94fc527
Robert Atkinson <bob@theatkinsons.org>
2019-10-29 15:50:37 -0700

    push adapted to rgatkinson

--- push.cmd
+++ push.cmd
@@ -1,7 +1,13 @@
 ::
 :: push.cmd
 ::
-ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org pip uninstall -y rgatkinson-opentrons-enhancements
+:: note: afterwards, robot needs (aparently) to be restarted to pick up new packages
+::
+ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org pip uninstall -y rgatkinson
 ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org rm -rf /tmp/packaging
 scp -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no -r packaging root@clarence.lutzlab.org:/tmp
 ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org pip install --no-index /tmp/packaging
+
+:: ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org rm -rf /data/packages/usr/local/lib/python3.6/site-packages/rgatkinson-opentrons-enhancements
+:: ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org rm -rf /data/packages/usr/local/lib/python3.6/site-packages/rgatkinson
+:: scp -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no -r packaging/src/rgatkinson root@clarence.lutzlab.org:/data/packages/usr/local/lib/python3.6/site-packages/rgatkinson
