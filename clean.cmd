544fb615b453c9224fe2d8d78a70d5faed6f4a1d
Robert Atkinson <bob@theatkinsons.org>
2019-10-28 21:09:29 -0700

    a more reasonable push.cmd

--- clean.cmd
+++
@@ -1,5 +0,0 @@
-::
-:: clean.cmd
-::
-ssh -i c:\users\bob\clarence_ssh_key root@clarence.lutzlab.org rm -r /data/packages/usr/local/lib/python3.6/site-packages/atkinson
-ssh -i c:\users\bob\clarence_ssh_key root@clarence.lutzlab.org rm -r /data/packages/usr/local/lib/python3.6/site-packages/atkinson.py
--- push.cmd
+++ push.cmd
@@ -1,4 +1,7 @@
 ::
 :: push.cmd
 ::
-scp -i c:\users\bob\clarence_ssh_key -r atkinson root@clarence.lutzlab.org:/data/packages/usr/local/lib/python3.6/site-packages
+ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org pip uninstall -y rgatkinson-opentrons-enhancements
+ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org rm -rf /tmp/packaging
+scp -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no -r packaging root@clarence.lutzlab.org:/tmp
+ssh -i c:\users\bob\clarence_ssh_key -o StrictHostKeyChecking=no root@clarence.lutzlab.org pip install --no-index /tmp/packaging
