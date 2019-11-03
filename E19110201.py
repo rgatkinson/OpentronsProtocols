7d70ec5dc006289f02597f3b189ed2adb28bbe99
Robert Atkinson <bob@theatkinsons.org>
2019-11-03 10:11:01 -0800

    plate largest volume first. use two waters, both in 5mL tubes. alternate parity of water vs sample

--- E19110201.py
+++ E19110201.py
@@ -26,7 +26,8 @@ config.trash_control = True
 
 use_manual_mix = True
 stock_volume = 630  # only used if we're not manually mixing
-min_water_volume = 10000
+waterA_initial_volume = 5000
+waterB_initial_volume = 5000
 
 ########################################################################################################################
 # Labware
@@ -52,13 +53,15 @@ plate = labware_manager.load('biorad_96_wellplate_200ul_pcr', slot=6, label='pla
 trough = labware_manager.load('usascientific_12_reservoir_22ml', slot=9, label='trough')
 
 # Name specific places in the labware containers
-water = trough['A1']
+waterA = eppendorf_5_0_rack['C4']
+waterB = eppendorf_5_0_rack['C5']
 initial_stock = eppendorf_1_5_rack['A1']
 dilutions = eppendorf_5_0_rack.rows['A']['1':2]
 
 # Remember initial liquid names and volumes
 log('Liquid Names')
-note_liquid(location=water, name='Water', min_volume=min_water_volume)
+note_liquid(location=waterA, name='Water', initial_volume=waterA_initial_volume)
+note_liquid(location=waterB, name='Water', initial_volume=waterB_initial_volume)
 note_liquid(location=initial_stock, name='AlluraRed', concentration="20.1442 mM", initial_volume=stock_volume)
 
 ########################################################################################################################
@@ -79,19 +82,20 @@ def make_dilution(water, source, dilution, dilution_volume, dilution_factor, man
         p50.transfer(dilution_water_volume, water, dilution, new_tip='once', trash=config.trash_control)
         p50.transfer(dilution_source_volume, source, dilution, new_tip='once', trash=config.trash_control)
 
-def make_dilutions():
+def make_dilutions(water):
     user_prompt('Make sure first dilution has been manually made')
     make_dilution(water, initial_stock, dilutions[0], 3500, 125, manual=use_manual_mix)
-    make_dilution(water, dilutions[0], dilutions[1], 3500, 625/125, manual=False)
+    make_dilution(water, dilutions[0], dilutions[1], 3500, 625 / 125, manual=False)
 
-def plate_dilution(source, stride, offset):
+def plate_dilution(source, stride, parity):
     # We have two dilutions, three replicates each, so get 16 volumes per
     volumes = [5, 10, 15, 20, 25, 30, 35, 50, 60, 70, 80, 90, 100, 125, 150, 175]
     num_reps = 3
-    for i, vol in enumerate(volumes):
+    for i, vol in enumerate(reversed(volumes)):
         log(f'Plating: source={source.get_name()} vol={vol}')
         row = i % 8
         col_first = int(i / 8)
+        offset = abs(parity - (i % 2))
         p = p10 if vol <= 10 else p50
         for j in range(num_reps):
             col = (col_first * num_reps + j) * stride + offset
@@ -106,14 +110,14 @@ def plate_dilution(source, stride, offset):
 # Off to the races
 ########################################################################################################################
 
-wells_to_verify = [dilutions[0], dilutions[1], water, plate.wells('A1'), plate.wells('A12'), plate.wells('H1'), plate.wells('H12')]
+wells_to_verify = [dilutions[0], dilutions[1], waterA, plate.wells('A1'), plate.wells('A12'), plate.wells('H1'), plate.wells('H12')]
 # verify_well_locations(wells_to_verify, p50)
 
 log('Making Dilutions')
-make_dilutions()
+make_dilutions(waterA)
 
 user_prompt('Mix dilutions by hand')
 
 log('Plating')
-plate_dilution(dilutions[1], stride=2, offset=0)
-plate_dilution(water, stride=2, offset=1)
+plate_dilution(dilutions[1], stride=2, parity=0)
+plate_dilution(waterB, stride=2, parity=1)
