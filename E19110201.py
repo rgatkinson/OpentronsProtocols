0f7548a3128446f1f63d0f8d3e8fe5b55d8b956c
Robert Atkinson <bob@theatkinsons.org>
2019-11-04 12:07:06 -0800

    nits

--- E19110201.py
+++ E19110201.py
@@ -20,8 +20,8 @@ from rgatkinson.pipette import verify_well_locations, instruments_manager
 ########################################################################################################################
 
 # Tip usage
-p10_start_tip = 'D5'
-p50_start_tip = 'G3'
+p10_start_tip = 'H9'
+p50_start_tip = 'A1'
 config.trash_control = True
 
 use_manual_mix = True
@@ -49,8 +49,7 @@ p50.start_at_tip(tips300a[p50_start_tip])
 # All the labware containers
 eppendorf_1_5_rack = labware_manager.load('opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap', slot=2, label='eppendorf_1_5_rack')
 eppendorf_5_0_rack = labware_manager.load('Atkinson 15 Tube Rack 5000 µL', slot=5, label='eppendorf_5_0_rack')
-plate = labware_manager.load('biorad_96_wellplate_200ul_pcr', slot=6, label='plateA')
-trough = labware_manager.load('usascientific_12_reservoir_22ml', slot=9, label='trough')
+plate = labware_manager.load('biorad_96_wellplate_200ul_pcr', slot=6, label='plate')
 
 # Name specific places in the labware containers
 waterA = eppendorf_5_0_rack['C4']
@@ -72,18 +71,18 @@ def make_dilution(water, source, dilution, dilution_volume, dilution_factor, man
     if manual:
         name = f'Dilution of {source.get_name()} by {dilution_factor}x'
         note_liquid(dilution, name, initial_volume=dilution_volume)
-    log(f'diluting from {source.get_name()} to {dilution.get_name()}')
+    log(f'{"Manually" if manual else "Automatically"} diluting from {source.get_name()} to {dilution.get_name()}')
     dilution_source_volume = dilution_volume / dilution_factor
     dilution_water_volume = dilution_volume - dilution_source_volume
     if manual:
         info(f'water vol={dilution_water_volume}')
         info(f'source vol={dilution_source_volume}')
+        user_prompt('')
     else:
         p50.transfer(dilution_water_volume, water, dilution, new_tip='once', trash=config.trash_control)
         p50.transfer(dilution_source_volume, source, dilution, new_tip='once', trash=config.trash_control)
 
 def make_dilutions(water):
-    user_prompt('Make sure first dilution has been manually made')
     make_dilution(water, initial_stock, dilutions[0], 3500, 125, manual=use_manual_mix)
     make_dilution(water, dilutions[0], dilutions[1], 3500, 625 / 125, manual=False)
 
