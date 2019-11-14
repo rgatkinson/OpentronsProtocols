3011b9a34c607e9affca2b70debe0f6badcb8405
Robert Atkinson <bob@theatkinsons.org>
2019-11-13 22:50:09 -0800

    version

--- packaging/setup.py
+++ packaging/setup.py
@@ -18,7 +18,7 @@ AUTHOR = 'Robert Atkinson'
 EMAIL = 'bob@theatkinsons.org'
 KEYWORDS = ["robots", "protocols", "synbio", "pcr", "automation", "lab", "opentrons"]
 DESCRIPTION = "Capability enhancements to the Opentrons API"
-VERSION = '1.0.6'
+VERSION = '1.0.7'
 PACKAGES = [DIST_NAME]
 CLASSIFIERS = [
     "Development Status :: 3 - Alpha",
