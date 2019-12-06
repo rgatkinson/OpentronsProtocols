rgatkinson/OpentronsProtocols

commit 5a09c61cf1a53f51625d3cd8a1300e0ac8c6c0e3

volume analyzing

packaging/src/rgatkinson/well.py
@@ -70,7 +70,7 @@ class WellGeometry(object):
         """
         How much can the well hold, in microliters? Default here to what Opentrons provides.
         """
-        result = self.well.max_volume() if self.well is not None else None
+        result = self.well.hi_volume() if self.well is not None else None
         if result is None:
             result = fpu.infinity
         return result

opentrons_analyze.py
@@ -14,9 +14,9 @@ from opentrons.legacy_api.containers import Slot
 from opentrons.legacy_api.containers.placeable import Placeable
 
 from rgatkinson.configuration import config, AbstractConfigurationContext
-from rgatkinson.interval import Interval
+from rgatkinson.interval import Interval, infimum
 from rgatkinson.liquid import Liquid, Concentration, Mixture, PipetteContents, LiquidVolume
-from rgatkinson.logging import Pretty, get_location_path, format_log_msg
+from rgatkinson.logging import Pretty, get_location_path, format_log_msg, pretty
 
 
 ########################################################################################################################
@@ -79,10 +79,11 @@ class WellAnalyzer(PlaceableAnalyzer):
             if self.liquid is not None:
                 result += ' ("{0:s}")'.format(self.liquid)
         result += ':'
-        result += Pretty().format(' lo={0:n} hi={1:n} cur={2:n} mix={3:s}\n',
-                                  self.liquid_volume.min_volume,
-                                  self.liquid_volume.max_volume,
+        result += pretty.format(' lo={0:n} hi={1:n} cur={2:n} taken={3:n} mix={4:s}\n',
+                                  self.liquid_volume.lo_volume,
+                                  self.liquid_volume.hi_volume,
                                   self.liquid_volume.current_volume,
+                                  infimum(self.liquid_volume.hi_volume) - infimum(self.liquid_volume.current_volume),
                                   self.mixture.__str__())
         return result
 

packaging/src/rgatkinson/liquid.py
@@ -325,11 +325,11 @@ class LiquidVolume(object):
         return max(0, self.current_volume_min - self._min_aspiratable_volume)
 
     @property
-    def min_volume(self):  # minimum historically seen
+    def lo_volume(self):  # minimum historically seen
         return self.initially + self.min_delta
 
     @property
-    def max_volume(self):  # maximum historically seen
+    def hi_volume(self):  # maximum historically seen
         return self.initially + self.max_delta
 
     @property

commit e71f6037ec688c4ce519753810a2f853932fe104

fix unbound method error introduced in 5a09c61c

packaging/src/rgatkinson/well.py
@@ -32,7 +32,7 @@ class WellGeometry(object):
     #-------------------------------------------------------------------------------------------------------------------
 
     def __init__(self, well=None):
-        self.__well = None
+        self.__well: Well = None
         self.well = well
 
     @property
@@ -70,7 +70,7 @@ class WellGeometry(object):
         """
         How much can the well hold, in microliters? Default here to what Opentrons provides.
         """
-        result = self.well.hi_volume() if self.well is not None else None
+        result = self.well.max_volume() if self.well is not None else None
         if result is None:
             result = infinity
         return result