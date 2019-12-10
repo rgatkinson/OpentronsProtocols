4e07c4c790a358c848a836ba435bfadab4abdb26
Robert Atkinson <bob@theatkinsons.org>
2019-12-10 13:42:39 -0800

    tidy dead code

--- packaging/src/rgatkinson/logging.py
+++ packaging/src/rgatkinson/logging.py
@@ -81,11 +81,8 @@ def silent_log(msg):
     pass
 
 def user_prompt(msg: str, prefix="***********", suffix=' ***********'):
-    from rgatkinson.pipette_v1 import instruments_manager
     if msg:
         log(msg=msg, prefix=prefix, suffix=suffix)
-    for pip in instruments_manager.instruments:
-        pass  # pip.retract()  # omit for now: Avoid 'Run encountered an error.'
     robot.pause('Press Return to Continue :-)')
 
 #-----------------------------------------------------------------------------------------------------------------------
