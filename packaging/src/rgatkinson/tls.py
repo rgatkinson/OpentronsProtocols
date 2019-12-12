cb1dba8d988a99c07dc73608abd0d5d98f20f874
Robert Atkinson <bob@theatkinsons.org>
2019-12-11 21:51:53 -0800

    more typing

--- packaging/src/rgatkinson/tls.py
+++ packaging/src/rgatkinson/tls.py
@@ -2,7 +2,6 @@
 # tls.py
 #
 import threading
-import typing
 
 import rgatkinson
 from rgatkinson.configuration import config
@@ -10,17 +9,17 @@ from rgatkinson.configuration import TopConfigurationContext
 # from rgatkinson.pipette import AspirateParamsTransfer, DispenseParamsTransfer, DispenseParams
 # from rgatkinson.pipettev2 import EnhancedPipetteV2
 
-# make thread local storage
 class TLS(threading.local):
     def __init__(self):
         # This gets called on every thread we're used on. Define default values
         self.update_pose_tree_in_place = False
         self.config: TopConfigurationContext = config
-        self.enhanced_pipette: typing.TypeVar
-        self.enhanced_pipette: 'EnhancedPipetteV2' = None
-        self.aspirate_params_transfer: 'AspirateParamsTransfer' = None
-        self.dispense_params_transfer: 'DispenseParamsTransfer' = None
-        self.dispense_params: 'DispenseParams' = None
+
+        # The typing on these still isn't right
+        self.enhanced_pipette: 'rgatkinson.pipettev2.EnhancedPipetteV2' = None
+        self.aspirate_params_transfer: 'rgatkinson.pipette.AspirateParamsTransfer' = None
+        self.dispense_params_transfer: 'rgatkinson.pipette.DispenseParamsTransfer' = None
+        self.dispense_params: 'rgatkinson.pipette.DispenseParams' = None
 
 tls: TLS = TLS()
 
